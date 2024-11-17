e23ae7b127bf5e29e8fa5c87b8ad499fdf00ec67
chrismrkr <b635032_@daum.net>
2024-11-17 18:39:18 +0900

    add devops

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ const App = () => {
   return (
     <div className="container">
       <div class="button">
-        <a href="http://localhost:8080/api/oauth2/authorize/kakao" className="kakao-login">카카오로 로그인하기</a>
+        <a href="http://localhost:80/api/oauth2/authorize/kakao" className="kakao-login">카카오로 로그인하기</a>
       </div>
     </div>
   );
 }
-
-
 
 export default App;
